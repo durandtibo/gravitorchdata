132a4412b3acbaeae7d2613b2d631aab8d9851d5
Thibaut Durand <durand.tibo@gmail.com>
2023-11-28 15:54:51 +1300

    Add `StartorchExampleDataPipeCreator` (#18)

---
+++ src/gravitorchdata/creators/datapipe/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+__all__ = ["StartorchExampleDataPipeCreator"]
+
+from gravitorchdata.creators.datapipe.startorch_example import (
+    StartorchExampleDataPipeCreator,
+)
